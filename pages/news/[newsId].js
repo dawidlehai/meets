0d54ea62ab1758ce6e6a41b2e2ba8549934bf036
dawidlehai <haidawidle@gmail.com>
2022-12-17 16:22:40 +0100

    Prepared files for a new project

--- pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const NewsDetails = () => {
-  const router = useRouter();
-  const { newsId } = router.query;
-  return <h1>News Details Page for {newsId}</h1>;
-};
-
-export default NewsDetails;
