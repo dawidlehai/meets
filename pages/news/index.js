f0130e10e377411ca0b5bb156b4ad4a4bfeb6297
dawidlehai <haidawidle@gmail.com>
2022-12-17 16:17:20 +0100

    Added Link's

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const News = () => {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/article1">Article 1</Link>
+        </li>
+        <li>
+          <Link href="/news/article2">Article 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default News;
