59fdb9d57c0394e73c1d385e2c5b67c82a037b11
dawidlehai <haidawidle@gmail.com>
2022-12-17 18:21:56 +0100

    Added meetups data

---
+++ data/meetups.js
@@ -0,0 +1,29 @@
+const meetups = [
+  {
+    id: "m1",
+    title: "React meetup in Berlin",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Cityscape_Berlin.jpg/1920px-Cityscape_Berlin.jpg",
+    address: "Wunderwaffe 44, 94223 Berlin",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: "m2",
+    title: "Our biggest Remix meetup in Bydgoszcz",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Bdg_NoweSpichrze_7_07-2013.jpg/1280px-Bdg_NoweSpichrze_7_07-2013.jpg",
+    address: "Dworcowa 21, 85090 Bydgoszcz",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    id: "m3",
+    title: "Learn JS together: A meetup in Poznań",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Stare_Miasto_w_Poznaniu.jpg/1920px-Stare_Miasto_w_Poznaniu.jpg",
+    address: "Gnieźnieńska 8, 23411 Poznań",
+    description:
+      "Eget dolor morbi non arcu risus. Urna cursus eget nunc scelerisque viverra mauris in. Mus mauris vitae ultricies leo integer malesuada nunc vel.",
+  },
+];
