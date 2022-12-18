cc6f40f6b35e5dc7b9e53e4bf635db9b4fb583d8
dawidlehai <haidawidle@gmail.com>
2022-12-18 19:40:39 +0100

    Loading details from DB

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,3 +1,6 @@
+import { MongoClient, ObjectId } from "mongodb";
+import { DB_CREDENTIALS } from "../config.js";
+
 import MeetupDetails from "../components/meetups/MeetupDetails";
 
 const MeetupDetailsPage = (props) => {
@@ -12,35 +15,42 @@ const MeetupDetailsPage = (props) => {
 };
 
 export const getStaticPaths = async () => {
+  const client = await MongoClient.connect(DB_CREDENTIALS);
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    })),
   };
 };
 
 export const getStaticProps = async (context) => {
   const { meetupId } = context.params;
+  const client = await MongoClient.connect(DB_CREDENTIALS);
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Bdg_NoweSpichrze_7_07-2013.jpg/1280px-Bdg_NoweSpichrze_7_07-2013.jpg",
-        id: meetupId,
-        title: "Our biggest Remix meetup in Bydgoszcz",
-        address: "Dworcowa 21, 85090 Bydgoszcz",
-        description:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: selectedMeetup.image,
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
