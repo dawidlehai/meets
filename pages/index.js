58ff622747e2191dcfca7b0fb3f82eb8ac4f8002
dawidlehai <haidawidle@gmail.com>
2022-12-18 08:30:32 +0100

    Added getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import MeetupList from "../components/meetups/MeetupList";
 import meetups from "../data/meetups";
 
-const HomePage = () => {
-  return <MeetupList meetups={meetups} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      meetups,
+    },
+  };
 };
 
 export default HomePage;
