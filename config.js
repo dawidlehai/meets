a9836b85aa5f48d1946ce3793b84d28ad36952aa
dawidlehai <haidawidle@gmail.com>
2022-12-18 14:36:57 +0100

    Added MongoDB connection

---
+++ config.js
@@ -0,0 +1,2 @@
+export const DB_CREDENTIALS =
+  "mongodb+srv://admin:177Whd5gKe8jtZOZ@cluster0.b7njjzr.mongodb.net/meets?retryWrites=true&w=majority";
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,20 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+import { DB_CREDENTIALS } from "../../config.js";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(DB_CREDENTIALS);
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 };
 
